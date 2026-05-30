1cc89930566a2c6d697f01303d41955d361c0686
Tejash Mehta <freakytejash>
2026-05-30 08:04:56 +0200

    Day 3: lists loops functions

--- marks.py
+++ marks.py
@@ -1,17 +1,21 @@
-s1 = int(input("Subject 1 marks: "))
-s2 = int(input("Subject 2 marks: "))
-s3 = int(input("Subject 3 marks: "))
-total = s1 + s2 + s3
-print("Total marks:", total)
-percentage = (total / 300) * 100
-print("Percentage:", percentage, "%")
-if percentage >= 90:
-    print("Grade: A")
-elif percentage >= 80:
-    print("Grade: B")
-elif percentage >= 70:
-    print("Grade: C")
-elif percentage >= 60:
-    print("Grade: D")
-else:
-    print("Grade: F")
+students = ["Dipak", "Kartik", "Sakshi", "Dhanashri"]
+
+# Define function
+def greet(name):
+    print(f"Hello, {name}! Welcome to the programming world.")
+
+greet("Sakshi")
+greet("Dipak")
+greet("Kartik")
+greet("Dhanashri")
+
+for student in students:
+    greet(student)
+
+
+
+
+
+
+
+
