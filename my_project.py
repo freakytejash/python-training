c59830b945cf904c05b3aa42ecaf6a5dc9239f12
Tejash Mehta <freakytejash>
2026-06-03 08:28:43 +0200

    Day 6: template jinja1 basde.html url_for

--- my_project.py
+++ my_project.py
@@ -1,10 +1,4 @@
-# What is Flask?
-# Flask is a micro web framework written in Python. It is designed to be lightweight and easy
-
-#Flask concepts:
-#Browser (Chrome) --> request --> Flask --> Python code
-#Browser (Chrome) <-- response <-- Flask <-- Python code
-
-# @app.route('/') --> This handles URL.
-# def home():  --> this is function.
-# return. ... --> this will take to
+# Minimum 3 routes
+# Homepage  Description about project
+# Records List iof dictionary
+# 1 more extra route according to your Project
---
+++ sample.py
@@ -0,0 +1,11 @@
+College Portal
+
+Home  | About | Students   | Teachers | Notices
+
+Shop
+Home | Products | Sales | Add products | About | Contact
+
+Canteen
+Home | Menu | About | Contact | Orders
+
+
